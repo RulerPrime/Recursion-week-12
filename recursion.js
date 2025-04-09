a4b5a614f14b3946b91b9f59d21063f501ebbcc9
RulerPrime <irletrondan@gmail.com>
2025-04-09 12:52:08 -0400

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -1,8 +1,23 @@
-
 //1. Make a function that computes a factorial recursively
 
-
-
+const factorial = function (num) {
+  //base case
+  if (num == 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+  //
+};
 
 //2. Make a function that recursively transposes a MIDI pitch down until it is in C Major
 
+const cMajor = [0, 2, 4, 5, 7, 9, 11];
+
+const scaleMap = function (midiIn) {
+  if (cMajor.includes(midiIn % 12)) {
+    return midiIn;
+  } else {
+    return scaleMap(midiIN - 1);
+  }
+};
